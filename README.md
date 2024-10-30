f85522ca6c58b5ecb11ceb10f0ae14f269fdef7d
Pim Dankloff <pimdankloff@gmail.com>
2024-10-30 17:23:35 +0100

    final format?

--- README.md
+++ README.md
@@ -20,7 +20,12 @@
 > Literature about different techniques to measure the critical micelle concentration.
 ### Force-based Tensiometry
 ### Image-based Tensiometry
+[Measurement of surface and interfacial tension using pendant drop tensiometry](https://www.sciencedirect.com/science/article/pii/S002197971500466X). Berry, Joseph D.; Neeson, Michael J.; Dagastine, Raymond R.; Chan, Derek Y. C.; Tabor, Rico F.. *Journal of Colloid and Interface Science* **454**, 226--237. , 2015.
 
+*notes*
+  - pendant drop
+  - discusses drop size & evaporation
+* * * * * * * * * * *  
 ### Refractive index
 [Rapid method for measurement of surface tension in multiwell plates](https://www.sciencedirect.com/science/article/pii/S0023683722022413). Cottingham, M. G.; Bain, C. D.; Vaux, D. J. T. *Laboratory Investigation* **84**(4), 523–529. Elsevier, 2004.
 
@@ -35,7 +40,7 @@
 [A model for monomer and micellar concentrations in surfactant solutions: Application to conductivity, NMR, diffusion, and surface tension data](https://www.sciencedirect.com/science/article/pii/S0021979711015268). Al-Soufi, W.; Piñeiro, L.; Novo, M. *Journal of Colloid and Interface Science* **370**(1), 102–110, 2012.  
 
 *notes*
-  - contains model for free surfactant concentration  
+  - model for free surfactant concentration  
 * * * * * * * * * * *  
 [Machine learning hybrid approach for the prediction of surface tension profiles of hydrocarbon surfactants in aqueous solution](https://doi.org/10.1016/j.jcis.2022.05.035). Seddon, D.; Müller, E. A.; Cabral, J. T. *Journal of Colloid and Interface Science* **625**, 328–339. Elsevier, 2022.  
   
@@ -53,4 +58,6 @@
 [Surfactant-{Specific} {AI}-{Driven} {Molecular} {Design}: {Integrating} {Generative} {Models}, {Predictive} {Modeling}, and {Reinforcement} {Learning} for {Tailored} {Surfactant} {Synthesis}](https://doi.org/10.1021/acs.iecr.4c00401). Nnadili, Miriam; Okafor, Andrew N.; Olayiwola, Teslim; Akinpelu, David; Kumar, Revati; Romagnoli, Jose A.. *Industrial \& Engineering Chemistry Research* **63**(14), 6313--6324. , 2024.
 
 *notes*
+  - gen AI
+
 
--- bib2md.py
+++ bib2md.py
@@ -22,31 +22,31 @@ def bibtex_to_markdown(bibtex_entry):
     volume_number = f"**{volume}**" + (f"({number})" if number else "")
 
     # Construct the Markdown citation
-    citation = f"- [{title}]({url}). {authors}. *{journal}* {volume_number}, {pages}. {publisher}, {year}."
+    citation = f"[{title}]({url}). {authors}. *{journal}* {volume_number}, {pages}. {publisher}, {year}.\n*notes*\n  -\n* * * * * * * * * * *"
     pyperclip.copy(citation)
 
 
 # Example usage
 bibtex_entry = """
 
-@article{nnadili_surfactant-specific_2024,
-	title = {Surfactant-{Specific} {AI}-{Driven} {Molecular} {Design}: {Integrating} {Generative} {Models}, {Predictive} {Modeling}, and {Reinforcement} {Learning} for {Tailored} {Surfactant} {Synthesis}},
-	volume = {63},
-	issn = {0888-5885},
-	shorttitle = {Surfactant-{Specific} {AI}-{Driven} {Molecular} {Design}},
-	url = {https://doi.org/10.1021/acs.iecr.4c00401},
-	doi = {10.1021/acs.iecr.4c00401},
-	abstract = {Molecular design is a critical aspect of various scientific and industrial fields, where the properties of molecules hold significant importance. In this study, a 3-fold methodology design is presented that leverages the power of generative artificial intelligence (AI), predictive modeling, and reinforcement learning to create tailored molecules with desired properties. This model synergistically combines deep learning techniques with Self-Referencing Embedded Strings (SELFIES) molecular representation to build a generative model that generates valid molecules and a graphical neural network model that accurately forecasts molecular properties. The Variational Autoencoder (VAE) coupled with reinforcement learning helps refine molecule generation based on targeted attributes. Data from an experimental study involving surfactants were used to test the framework. A validation of the structural integrity of the molecules generated was conducted, and Tanimoto similarities were used to quantify the similarity and diversity between the original and generated molecular structures. Also, saliency maps for the generated surfactants were produced to identify the features explaining the property values. Lastly, molecular dynamics simulations were used to validate the stability of the generated molecules. The results showed that the proposed framework can effectively produce valid molecules within the set property threshold value.},
-	number = {14},
-	urldate = {2024-08-05},
-	journal = {Industrial \& Engineering Chemistry Research},
-	author = {Nnadili, Miriam and Okafor, Andrew N. and Olayiwola, Teslim and Akinpelu, David and Kumar, Revati and Romagnoli, Jose A.},
-	month = apr,
-	year = {2024},
-	note = {Publisher: American Chemical Society},
-	pages = {6313--6324},
-	file = {Nnadili et al_2024_Surfactant-Specific AI-Driven Molecular Design.pdf:C\:\\Users\\pimda\\Zotero\\storage\\8SIMF5G6\\Nnadili et al_2024_Surfactant-Specific AI-Driven Molecular Design.pdf:application/pdf},
+
+@article{berry_measurement_2015,
+	title = {Measurement of surface and interfacial tension using pendant drop tensiometry},
+	volume = {454},
+	issn = {0021-9797},
+	url = {https://www.sciencedirect.com/science/article/pii/S002197971500466X},
+	doi = {10.1016/j.jcis.2015.05.012},
+	abstract = {Pendant drop tensiometry offers a simple and elegant solution to determining surface and interfacial tension – a central parameter in many colloidal systems including emulsions, foams and wetting phenomena. The technique involves the acquisition of a silhouette of an axisymmetric fluid droplet, and iterative fitting of the Young–Laplace equation that balances gravitational deformation of the drop with the restorative interfacial tension. Since the advent of high-quality digital cameras and desktop computers, this process has been automated with high speed and precision. However, despite its beguiling simplicity, there are complications and limitations that accompany pendant drop tensiometry connected with both Bond number (the balance between interfacial tension and gravitational forces) and drop volume. Here, we discuss the process involved with going from a captured experimental image to a fitted interfacial tension value, highlighting pertinent features and limitations along the way. We introduce a new parameter, the Worthington number, Wo, to characterise the measurement precision. A fully functional, open-source acquisition and fitting software is provided to enable the reader to test and develop the technique further.},
+	urldate = {2024-09-30},
+	journal = {Journal of Colloid and Interface Science},
+	author = {Berry, Joseph D. and Neeson, Michael J. and Dagastine, Raymond R. and Chan, Derek Y. C. and Tabor, Rico F.},
+	month = sep,
+	year = {2015},
+	keywords = {Interfacial tension, Surface tension, Bond number, Drop shape analysis, Pendant drop, Tensiometry},
+	pages = {226--237},
+	file = {ScienceDirect Snapshot:C\:\\Users\\pimda\\Zotero\\storage\\S7PK5ZYH\\S002197971500466X.html:text/html},
 }
+
 
 """
 
